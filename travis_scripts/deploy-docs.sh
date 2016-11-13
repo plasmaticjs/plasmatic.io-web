7a00ecab040051e3159e4dd134844d068cc0d296
Milos Mosovsky <milos.mosovsky@toptal.com>
2016-11-13 05:55:58 +0100

    fixup! Initial commit

--- travis_scripts/deploy-docs.sh
+++ travis_scripts/deploy-docs.sh
@@ -17,3 +17,11 @@ git init
 git add .
 git commit -m "Deploy Documentation from Travis CI"
 git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+
+# Purge cloudflare!
+
+curl -X DELETE "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE_ID}/purge_cache" \
+     -H "X-Auth-Email: ${CLOUDFLARE_USER_EMAIL}" \
+     -H "X-Auth-Key: ${CLOUDFLARE_API_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"purge_everything":true}'
